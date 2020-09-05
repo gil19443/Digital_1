9e91149137b7a5446d0936e84f60effafd28c6f2
gil19443 <gil19443@uvg.edu.gt>
2020-09-05 12:58:12 -0600

    correcciones lab06

--- Laboratorios/Lab06/ejercicio5y6_tb.v
+++ Laboratorios/Lab06/ejercicio5y6_tb.v
@@ -8,26 +8,21 @@ reg A, B, p3;
 reg clock, RST, ST;
 
 FFD5      U1(clock, RST,ST, D1, Q1);
-FINALEJ1  U3(p1, p2, clock, RST, O0);
+FINALEJ1  U3(A, B, clock, RST, O0);
 FINALEJ3  U4(p3, clock, RST, O3, O2, O1);
 
 always
-  begin
-  clock <=0;
-  #1 clock <=~clock; #1 ;
-  end
-always
-  begin
-    ST <=0;
-    #1 ST <=~ST; #1 ;
-    end
+  #5 clock <=~clock;
+
+  always
+    #1 ST <=~ST;
 //testbench flip flop d con reset asincrono
   initial begin
   $display(" Prueba ffrst");
   $display("S C R  D    |  Q ");
   $display("------------|-----");
   $monitor("%b %b %b %b  | %b  ", ST,clock, RST, D1[3:0], Q1[3:0]);
-     RST = 1; D1[3] = 1; D1[2] = 1; D1[1] = 0; D1[0] = 0;
+     RST = 1; D1[3] = 1; D1[2] = 1; D1[1] = 0; D1[0] = 0; clock = 0;
   #3 RST = 0; D1[3] = 0; D1[2] = 1; D1[1] = 1; D1[0] = 0;
   #3 RST = 0; D1[3] = 1; D1[2] = 1; D1[1] = 1; D1[0] = 1;
   #3 RST = 0; D1[3] = 0; D1[2] = 0; D1[1] = 1; D1[0] = 1;
@@ -39,47 +34,51 @@ always
   $display(" EJERCICIO 1 ");
   $display("clk A B | Y ");
   $display("--------|---");
-  $monitor("%b  %b | %b " , A, B, O0);
-  A = 0; B = 0;
-#2	A = 0; B = 1;
-#2	A = 1; B = 0;
-#2	A = 1; B = 1;
-#2	A = 0; B = 1;
-#2	A = 0; B = 0;
-#2	A = 0; B = 1;
-#2	A = 1; B = 0;
-#2	A = 1; B = 1;
-#2	A = 0; B = 1;
+  $monitor("%b  %b %b | %b  " ,clock, A, B, O0);
+  end
+  initial begin
+    A = 0; B = 0; RST = 0; clock = 0;
+    #1	RST = 1;
+    #1	RST = 0;
+    #10
+    A = 1; B = 0;
+    #20
+    A = 0; B = 0;
+    #10
+    A = 1; B = 1;
   end
 
   initial begin
-  #41
+  #56
   $display(" EJERCICIO 3 ");
   $display("clk p |  Y ");
   $display("------|----");
   $monitor("%b  %b  | %b %b %b " ,clock,p3, O3,O2,O1);
-     p3 = 1; RST = 1;
-     p3 = 1; RST = 0;
-     p3 = 1;
-  #2 p3 = 1;
-  #2 p3 = 1;
-  #2 p3 = 1;
-  #2 p3 = 1;
-  #2 p3 = 1;
-  #2 p3 = 1;
-  #2 p3 = 1;
-  #2 p3 = 0;
-  #2 p3 = 0;
-  #2 p3 = 0;
-  #2 p3 = 0;
-  #2 p3 = 0;
-  #2 p3 = 0;
-  #2 p3 = 0;
-  #2 p3 = 0;
+  end
+  initial begin
+  #57
+     p3 = 1; RST = 1; 
+  #1 p3 = 1; RST = 0;
+  #1 p3 = 1;
+  #1 p3 = 1;
+  #1 p3 = 1;
+  #1 p3 = 1;
+  #1 p3 = 1;
+  #1 p3 = 1;
+  #1 p3 = 1;
+  #1 p3 = 1;
+  #1 p3 = 0;
+  #1 p3 = 0;
+  #1 p3 = 0;
+  #1 p3 = 0;
+  #1 p3 = 0;
+  #1 p3 = 0;
+  #1 p3 = 0;
+  #1 p3 = 0;
   end
 
   initial
-    #73 $finish;
+    #100 $finish;
   initial begin
       $dumpfile("ejercicio5y6_tb.vcd");
       $dumpvars(0, testbench);
